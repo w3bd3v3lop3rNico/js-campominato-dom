a9578fa3145459c6d1fde2729e5981ac1e28f58f
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-16 17:16:46 +0200

    Start write array random number function

--- js/bonus.js
+++ js/bonus.js
@@ -1,7 +1,5 @@
 const playBtnDOMElement = document.getElementById('play-btn');
 
-// - Cambiare tabella in base alla difficoltà
-//     - Recupero dal DOM l'elemento select ed il suo valore
 const selectModeDOMElement = document.getElementById('mode-select');
 // console.log(selectModeDOMElement);
 
@@ -14,6 +12,8 @@ playBtnDOMElement.addEventListener('click', function() {
     } else if (selectModeDOMElement.value === '3') {
         gridGen(49, "cell hard-grid")
     }
+    getArrayRandomFromARange(1, 100, 16)
+    console.log(getArrayRandomFromARange)
     
 
     const cellsDOMElements = document.querySelectorAll('.cell');
@@ -47,3 +47,30 @@ function gridGen(cellsNumb, difficultyClass) {
         console.log(difficultyClass);
     }
 }
+
+// - Generare 16 bombe
+//     - dichiaro una funzione che generi numeri random da 1 ad un argomento range-max
+function getRandomInt(minRange, maxRange) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+//     - Dichiaro una funzione contenente un'array vuoto che dovrà essere riempito successivamente
+function getArrayRandomFromARange(minRange, maxRange, number){
+    const bombsArray = [];
+    
+}
+//     - inizializzo un ciclo while che mi genera 16 
+//     numeri random in base al range di celle
+//     - invoco la funzione che genera i numeri random
+//     - mi accerto che i numeri generati non siano uguali
+//         - SE i numeri generati sono uguali ad almeno un numero all'interno dell'array il ciclo continua senza leggere il codice
+//         - ALTRIMENTI il numero generato viene pushato nell'array
+// - Al click dell'utente su una cella determinare se ha trovato una bomba o no
+//     - SE l'utente trova una bomba
+//         - coloro la cella di rosso
+//         - la pagina avvisa l'utente del game over
+//     - ATRIMENTI il gioco prosegue
+//         - viene incrementato il punteggio
+//         - coloro le celle di azzuro
+//         - verifico se il giocatore ha finito il gioco
